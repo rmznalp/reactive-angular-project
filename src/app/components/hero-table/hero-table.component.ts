5d7a675f06be7f961e6ddc2832b11e82618c1641
ramazan.alp <ramazan.alp@pia-team.com>
2021-09-13 21:51:46 +0100

    changing to vm model

--- src/app/components/hero-table/hero-table.component.ts
+++ src/app/components/hero-table/hero-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Hero, HeroService } from '../../services/hero.service';
 
 @Component({
@@ -7,11 +9,27 @@ import { Hero, HeroService } from '../../services/hero.service';
     styleUrls: ['./hero-table.component.scss'],
 })
 export class HeroTableComponent implements OnInit {
-    heroes$ = this.hero.heroes$;
-    search$ = this.hero.searchBs;
-    page$ = this.hero.userPage$;
-    totalResults$ = this.hero.totalResults$;
-    totalPages$ = this.hero.totalPages$;
+    vm$ = combineLatest([
+        this.hero.heroes$,
+        this.hero.searchBs,
+        this.hero.userPage$,
+        this.hero.limitBs,
+        this.hero.totalResults$,
+        this.hero.totalPages$,
+    ]).pipe(
+        map(([heroes, search, page, limit, totalResults, totalPages]) => {
+            return {
+                heroes,
+                search,
+                page,
+                limit,
+                totalResults,
+                totalPages,
+                disableNext: totalPages === page,
+                disablePrev: page === 1,
+            };
+        }),
+    );
 
     constructor(public hero: HeroService) {}
 
@@ -19,10 +37,16 @@ export class HeroTableComponent implements OnInit {
 
     doSearch(event: any) {
         this.hero.searchBs.next(event.target.value);
+        this.hero.pageBs.next(0);
     }
 
     movePageBy(moveBy: number) {
         const currenPage = this.hero.pageBs.getValue();
         this.hero.pageBs.next(currenPage + moveBy);
+    }
+
+    setLimit(limit: number) {
+        this.hero.limitBs.next(limit);
+        this.hero.pageBs.next(0);
     }
 }
